40e6b3c7474f929c0908eb241871c0bcb75985ce
Jack Berg <johnmberg8@gmail.com>
2025-12-29 15:24:17 -0600

    Limit all_auto_instrumentation_agents_env_path env vars to entries prefixed with OTEL_

--- src/config.zig
+++ src/config.zig
@@ -13,6 +13,7 @@ const testing = std.testing;
 const config_file_path = "/etc/opentelemetry/otelinject.conf";
 const max_line_length = 8192;
 const empty_string = @constCast("");
+const otel_env_var_prefix = "OTEL_";
 
 const dotnet_path_key = "dotnet_auto_instrumentation_agent_path_prefix";
 const jvm_path_key = "jvm_auto_instrumentation_agent_path";
@@ -169,6 +170,11 @@ fn readConfigurationFile(cfg_file_path: []const u8, configuration: *InjectorConf
 }
 
 fn applyKeyValueToAllAgentsEnv(key: []const u8, value: []u8, _file_path: []const u8, _configuration: *InjectorConfiguration) void {
+    if (!std.mem.startsWith(u8, key, otel_env_var_prefix)) {
+        print.printError("env var {s} does not start with {s}. ignoring.", .{ key, otel_env_var_prefix });
+        return;
+    }
+
     _configuration.all_auto_instrumentation_agents_env_vars.put(key, value) catch |e| {
         print.printError("error storing environment variable {s} from file {s}: {}", .{ key, _file_path, e });
     };

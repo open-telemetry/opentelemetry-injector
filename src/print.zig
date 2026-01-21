open-telemetry/opentelemetry-injector

commit 8f1535a9e19d922e3c920699098a4e44098a2bf1

feat: add PID to log output (#200)
* add pid to the log output
* fix memory leak with exe path and arguments

src/print.zig
@@ -223,5 +223,6 @@ pub fn printError(comptime fmt: []const u8, args: anytype) void {
 }
 
 fn _printMessage(comptime fmt: []const u8, args: anytype) void {
-    std.debug.print(log_prefix ++ fmt ++ "\n", args);
+    const pid: u32 = @intCast(std.os.linux.getpid());
+    std.debug.print(log_prefix ++ "[{d:>7}] " ++ fmt ++ "\n", .{pid} ++ args);
 }

src/root.zig
@@ -191,11 +191,17 @@ fn evaluateAllowDeny(allocator: std.mem.Allocator, configuration: config.Injecto
         // getExecutablePath.
         return true;
     };
+    defer allocator.free(exe_path);
+
     const args = getCommandLineArgs(allocator) catch {
         // Skip allow-deny evaluation if getting the arguments has failed. The error has already been logged in
         // getCommandLineArgs.
         return true;
     };
+    defer {
+        for (args) |arg| allocator.free(arg);
+        allocator.free(args);
+    }
 
     var allow = (configuration.include_paths.len == 0) or pattern_matcher.matchesAnyPattern(exe_path, configuration.include_paths);
     allow = allow or (configuration.include_args.len == 0) or pattern_matcher.matchesManyAnyPattern(args, configuration.include_args);

.chloggen/pid_cmdline_logs.yaml
@@ -1,0 +1,27 @@
+# Use this changelog template to create an entry for release notes.
+
+# One of 'breaking', 'deprecation', 'new_component', 'enhancement', 'bug_fix'
+change_type: enhancement
+
+# The name of the component, or a single word describing the area of concern, (e.g. filelogreceiver)
+component: logging
+
+# A brief description of the change.  Surround your text with quotes ("") if it needs to start with a backtick (`).
+note: Adds process id to each log line
+
+# Mandatory: One or more tracking issues related to the change. You can use the PR number here if no issue exists.
+issues: [200]
+
+# (Optional) One or more lines of additional information to render under the primary note.
+# These lines will be padded with 2 spaces and then inserted directly into the document.
+# Use pipe (|) for multiline entries.
+subtext:
+
+# If your change doesn't affect end users or the exported elements of any package,
+# you should instead start your pull request title with "chore" or use the "Skip Changelog" label.
+# Optional: The change log or logs in which this entry should be included.
+# e.g. '[user]' or '[user, api]'
+# Include 'user' if the change is relevant to end users.
+# Include 'api' if there is a change to a library API.
+# Default: '[user]'
+change_logs: [user]

commit b3257a5738098192c546cf8a89414d8c9aff1388

chore: init pid for log messages only once
Commit 8f1535a9e19d922e3c920699098a4e44098a2bf1 introduced a change that
crashes the Zig unit tests when executed directly on MacOS - in
particular the std.os.linux.getpid() crashes when the OS is not Linux.

While support for any OS besides Linux is currently not a goal of this
project, enabling developers to run the Zig unit tests directly on their
native OS has value.

This commit also changes the initialization in src/print.zig to acquire
the pid once, and then reuse that value when printing log messages.

src/print.zig
@@ -1,6 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
+const builtin = @import("builtin");
 const std = @import("std");
 
 const testing = std.testing;
@@ -19,6 +20,8 @@ const LogLevel = enum(u8) {
 
 var log_level: LogLevel = .Error;
 
+var pid: u32 = 0;
+
 const proc_self_environ_path = "/proc/self/environ";
 
 /// Initializes the log level based on the environment variable OTEL_INJECTOR_LOG_LEVEL, by reading /proc/self/environ
@@ -32,6 +35,16 @@ pub fn initLogLevelFromProcSelfEnviron() !void {
 
 // Note: initLogLevelFromEnvironFile is exposed as pub for testing purposes only.
 pub fn initLogLevelFromEnvironFile(self_environ_path: []const u8) !void {
+    pid = switch (builtin.target.os.tag) {
+        .linux => @intCast(std.os.linux.getpid()),
+        // Note: the injector does not support any OS besides Linux, this case is only here to support running Zig unit
+        // tests directly on Darwin.
+        .macos => @intCast(std.c.getpid()),
+        else => {
+            error.OsNotSupported;
+        },
+    };
+
     var log_level_env_var_value: ?[]const u8 = null;
 
     var environ_file = try std.fs.openFileAbsolute(self_environ_path, .{});
@@ -223,6 +236,5 @@ pub fn printError(comptime fmt: []const u8, args: anytype) void {
 }
 
 fn _printMessage(comptime fmt: []const u8, args: anytype) void {
-    const pid: u32 = @intCast(std.os.linux.getpid());
     std.debug.print(log_prefix ++ "[{d:>7}] " ++ fmt ++ "\n", .{pid} ++ args);
 }

src/root.zig
@@ -19,7 +19,9 @@ const empty_z_string = "\x00";
 
 const init_section_name = switch (builtin.target.os.tag) {
     .linux => ".init_array",
-    .macos => "__DATA,__mod_init_func", // needed to run tests locally on macOS
+    // Note: the injector does not support any OS besides Linux, this case is only here to support running Zig unit
+    // tests directly on Darwin.
+    .macos => "__DATA,__mod_init_func",
     else => {
         error.OsNotSupported;
     },